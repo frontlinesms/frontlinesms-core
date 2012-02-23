4c4249ff87bed109d9e4b52f3d7f515b4ebf672f
Alex Anderson <alex@frontlinesms.com>
2012-02-23 11:11:44 +0300

    Added closing of confirmation dialog when it is confirmed.

--- src/main/java/net/frontlinesms/ui/handler/core/ConfirmationDialogHandler.java
+++ src/main/java/net/frontlinesms/ui/handler/core/ConfirmationDialogHandler.java
@@ -11,11 +11,15 @@ import static thinlet.Thinlet.find;
 public class ConfirmationDialogHandler implements ThinletUiEventHandler {
 	private final FrontlineUI ui;
 	private final Object dialog;
+	private final String methodToBeCalled;
+	private final ThinletUiEventHandler handler;
 	
 	public ConfirmationDialogHandler(FrontlineUI ui, ThinletUiEventHandler handler, String methodToBeCalled) {
 		this.ui = ui;
 		dialog = ui.loadComponentFromFile(UI_FILE_CONFIRMATION_DIALOG_FORM, this);
-		ui.setMethod(find(dialog, COMPONENT_BT_CONTINUE), Thinlet.ATTRIBUTE_ACTION, methodToBeCalled, ui.getDesktop(), handler);
+		this.methodToBeCalled = methodToBeCalled;
+		this.handler = handler;
+		ui.setMethod(find(dialog, COMPONENT_BT_CONTINUE), Thinlet.ATTRIBUTE_ACTION, "doMethodCall", ui.getDesktop(), this);
 		ui.add(dialog);
 	}
 	
@@ -26,5 +30,12 @@ public class ConfirmationDialogHandler implements ThinletUiEventHandler {
 	
 	public void removeDialog(Object dialog) {
 		ui.remove(dialog);
+	}
+	
+	public void doMethodCall() {
+		Object button = find(dialog, COMPONENT_BT_CONTINUE);
+		ui.setMethod(button, Thinlet.ATTRIBUTE_ACTION, methodToBeCalled, ui.getDesktop(), handler);
+		ui.invokeAction(button);
+		removeDialog(dialog);
 	}
 }
